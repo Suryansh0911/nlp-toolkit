ca773c6c0b0a5be23568ecfc60f51a3b83a54aa8
Suryansh0911 <suryanshgupta0911@gmail.com>
2026-08-23 19:18:21 +0530

    fix path

--- backend/src/data/task_dataset.py
+++ backend/src/data/task_dataset.py
@@ -1,10 +1,12 @@
 from datasets import Dataset
 import json
 
-def load_raw_dataset(path: str = "C:/Users/surya/OneDrive/Desktop/nlp-toolkit/backend/data/raw/cs_examples.json") -> Dataset:
+def load_raw_dataset():
+    base_dir = Path(__file__).resolve().parents[2]
+    path = base_dir / "data" / "raw" / "cs_examples.json"
+
     with open(path, "r", encoding="utf-8") as f:
-        data = json.load(f)
-    return Dataset.from_list(data)
+        return json.load(f)
 
 
 def load_task_dataset(dataset : Dataset, task: str) -> Dataset:
